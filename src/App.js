3fa7c0e5d198d0afc942b0d27928ed7af1a6f521
That Andrew Guy <andrew.kulagin@student.uts.edu.au>
2024-11-23 13:45:42 +1100

    fixed build

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import FAQ from './pages/FAQ';
 import BoatHirePage from './pages/BoatHirePage';
 
 // Analytics Service
-const GA_MEASUREMENT_ID = G-G1Q96T1QXM;
+const GA_MEASUREMENT_ID = "G-G1Q96T1QXM";
 
 const loadGoogleAnalytics = () => {
   if (process.env.NODE_ENV === 'production') {
